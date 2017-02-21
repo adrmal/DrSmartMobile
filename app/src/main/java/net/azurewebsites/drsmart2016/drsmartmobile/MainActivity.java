d4fd7da48bf0a8393b2c9f2c36d90acd42a890b3
adrmal <adr_mal@wp.pl>
2017-02-21 16:41:17 +0100

    initial commit

---
+++ app/src/main/java/net/azurewebsites/drsmart2016/drsmartmobile/MainActivity.java
@@ -0,0 +1,13 @@
+package net.azurewebsites.drsmart2016.drsmartmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
