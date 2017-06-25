b2050fbe54dae098952de890093fa0492f773d05
Adrian Małolepszy <adr_mal@wp.pl>
2017-06-25 22:23:25 +0200

    WERSJA 1.0.0

--- app/src/main/java/net/azurewebsites/drsmart2016/drsmartmobile/view/activity/MainActivity.java
+++ app/src/main/java/net/azurewebsites/drsmart2016/drsmartmobile/view/activity/MainActivity.java
@@ -1,79 +1,65 @@
 package net.azurewebsites.drsmart2016.drsmartmobile.view.activity;
 
 import android.content.Intent;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.support.design.widget.NavigationView;
-import android.view.MenuItem;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import net.azurewebsites.drsmart2016.drsmartmobile.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout drawerLayout;
-    private Toolbar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ViewCompat.setElevation(toolbar, 4);
 
-        initializeNavigationDrawerFields();
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        initializeCardViews();
     }
 
-    private void initializeNavigationDrawerFields() {
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
-        drawerLayout.addDrawerListener(drawerToggle);
-        drawerToggle.syncState();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+    private void initializeCardViews() {
+        CardView cardViewVisits = (CardView) findViewById(R.id.visits);
+        cardViewVisits.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                doNavigationDrawerAction(item);
-                return true;
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VisitActivity.class);
+                startActivity(intent);
+            }
+        });
+        CardView cardViewMedicalHistory = (CardView) findViewById(R.id.medicalHistory);
+        cardViewMedicalHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MedicalHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+        CardView cardViewUserDetails = (CardView) findViewById(R.id.userDetails);
+        cardViewUserDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        CardView cardViewLogOut = (CardView) findViewById(R.id.logOut);
+        cardViewLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    private void doNavigationDrawerAction(MenuItem item) {
-        drawerLayout.closeDrawer(GravityCompat.START);
-        Intent intent;
-        switch(item.getItemId()) {
-            case R.id.drawerVisits:
-                intent = new Intent(this, VisitActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.drawerMedicalHistory:
-                intent = new Intent(this, MedicalHistoryActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.drawerUserDetails:
-                intent = new Intent(this, UserDetailsActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.drawerLogOut:
-                intent = new Intent(this, LoginActivity.class);
-                startActivity(intent);
-        }
-    }
-
     @Override
     public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else {
-            moveTaskToBack(true);
-        }
+        moveTaskToBack(true);
     }
 
 }
